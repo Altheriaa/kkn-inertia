5f3057f836e2ff2871c5f8d9f0e19bbc9013db0a
Altheriaa <daffaalfharizy265@gmail.com>
2026-03-24 08:22:09 +0700

    Coba lagi bug prod

--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -12,7 +12,11 @@ const props = defineProps({
 const formatDate = (dateStr) => {
     if (!dateStr) return '-';
     const date = new Date(dateStr);
-    return date.toLocaleDateString('id-ID', { day: '2-digit', month: 'short', year: 'numeric' });
+    return date.toLocaleDateString('id-ID', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric'
+    });
 };
 
 const now = new Date();
